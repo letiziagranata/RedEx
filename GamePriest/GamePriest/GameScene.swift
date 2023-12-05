ff31335d4638541fbfb0ecd3de72ec6051c712f8
ForegroundMattia <mmarranzino23@fed.idserve.net>
2023-12-05 11:47:29 +0100

    background

--- GamePriest/GamePriest/GameScene.swift
+++ GamePriest/GamePriest/GameScene.swift
@@ -15,6 +15,7 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
         
+        
         // Get label node from scene and store it for use later
         self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
         if let label = self.label {
