c4fdc1de9a067da426c80d652d595d84796e6b8f
DonatoBastione <dbastione23@fed.idserve.net>
2023-12-06 10:07:10 +0100

    armi

---
+++ GamePriest/GamePriest/Weapon.swift
@@ -0,0 +1,28 @@
+//
+//  Weapons.swift
+//  GamePriest
+//
+//  Created by DONATO BASTIONE on 05/12/23.
+//
+
+import Foundation
+import SpriteKit
+
+
+class Croce: SKSpriteNode{
+    var damege: Int = 10
+    var range: Int = 1
+    var sprite: String = ""
+}
+
+class Granata: SKSpriteNode{
+    var damege: Int = 5
+    var ragne: Int = 20
+    var sprite: String = ""
+}
+
+class Incensiere: SKSpriteNode{
+    var damege: Int = 1
+    var range: Int = 5
+    var sprite: String = ""
+}
