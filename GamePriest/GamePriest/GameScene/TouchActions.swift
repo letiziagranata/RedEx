f4dc7d44cef2afed56ee0481654b05977c2e8594
mariaconcettarena <mariaconcettaarena@hotmail.it>
2023-12-11 11:39:43 +0100

    riprovo

--- GamePriest/GamePriest/GameScene/TouchActions.swift
+++ GamePriest/GamePriest/GameScene/TouchActions.swift
@@ -35,9 +35,9 @@ extension GameScene {
     //TOCCO SUL PRETE: se sto continuando a tenerlo premuto
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         //guard isTouchingPriest, let touch = touches.first else { return }
-        guard !isPriestPaused, isTouchingPriest, let touch = touches.first else {
-               return
-           }
+        guard !isPriestPaused, isTouchingPriest, isMoving, let touch = touches.first else {
+                return
+            }
         let touchLocation = touch.location(in: self)
         
         // Chiamare la funzione move(to:) solo se il tocco è ancora sulla scena
