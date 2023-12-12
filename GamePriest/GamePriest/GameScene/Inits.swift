4db1470300eebe1d36e518d6c9f29c0c58e82263
mariaconcettarena <mariaconcettaarena@hotmail.it>
2023-12-12 10:51:14 +0100

    aggiustata grafica cuore

--- GamePriest/GamePriest/GameScene/Inits.swift
+++ GamePriest/GamePriest/GameScene/Inits.swift
@@ -221,9 +221,9 @@ extension GameScene {
         cuore = Heart()
         cuore.name = "cuore"
         cuore.zPosition = 5
-        cuore.position = CGPoint(x: -180, y: -400)
-        cuore.xScale = 0.5
-        cuore.yScale = 0.5
+        cuore.position = CGPoint(x: -180, y: -385)
+        cuore.xScale = 0.6
+        cuore.yScale = 0.6
         
         addChild(cuore)
     }
